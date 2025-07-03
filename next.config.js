8e8316d0bb7948ec9a12f0b2bc8d789516f3d283
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2025-07-03 16:06:40 +0000

    feat: Initial commit

---
+++ next.config.js
@@ -0,0 +1,78 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  eslint: {
+    dirs: ["src"],
+  },
+
+  reactStrictMode: true,
+  swcMinify: true,
+  
+  // Custom build ID for cache busting
+  generateBuildId: () => process.env.VERCEL_GIT_COMMIT_SHA,
+
+  // Add domain whitelist for images
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.guesty.com",
+      },
+      {
+        protocol: "https",
+        hostname: "apps.guesty.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: { not: /\.(css|scss|sass)$/ },
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        loader: "@svgr/webpack",
+        options: {
+          dimensions: false,
+          titleProp: true,
+        },
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+
+module.exports = nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,94 @@
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class", // Enable dark mode with class strategy
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Helvetica", "Arial", ...defaultTheme.fontFamily.sans],
+        athletic: ['"New Athletic M54"', "monospace"],
+      },
+      zIndex: {
+        "0": "0",
+        "5": "5",
+        "10": "10",
+        "15": "15",
+        "20": "20",
+        "25": "25",
+        "30": "30",
+        "35": "35",
+        "40": "40",
+        "45": "45",
+        "50": "50",
+        "55": "55",
+        "60": "60",
+        "65": "65",
+        "70": "70",
+        "75": "75",
+        "80": "80",
+        "85": "85",
+        "90": "90",
+        "95": "95",
+        "100": "100",
+      },
+      colors: {
+        primary: {
+          // Forest green - Customize it on globals.css :root
+          50: "rgb(var(--tw-color-primary-50) / <alpha-value>)",
+          100: "rgb(var(--tw-color-primary-100) / <alpha-value>)",
+          200: "rgb(var(--tw-color-primary-200) / <alpha-value>)",
+          300: "rgb(var(--tw-color-primary-300) / <alpha-value>)",
+          400: "rgb(var(--tw-color-primary-400) / <alpha-value>)",
+          500: "rgb(var(--tw-color-primary-500) / <alpha-value>)",
+          600: "rgb(var(--tw-color-primary-600) / <alpha-value>)",
+          700: "rgb(var(--tw-color-primary-700) / <alpha-value>)",
+          800: "rgb(var(--tw-color-primary-800) / <alpha-value>)",
+          900: "rgb(var(--tw-color-primary-900) / <alpha-value>)",
+          950: "rgb(var(--tw-color-primary-950) / <alpha-value>)",
+        },
+        secondary: {
+          // Dark grey
+          50: "rgb(var(--tw-color-secondary-50) / <alpha-value>)",
+          100: "rgb(var(--tw-color-secondary-100) / <alpha-value>)",
+          200: "rgb(var(--tw-color-secondary-200) / <alpha-value>)",
+          300: "rgb(var(--tw-color-secondary-300) / <alpha-value>)",
+          400: "rgb(var(--tw-color-secondary-400) / <alpha-value>)",
+          500: "rgb(var(--tw-color-secondary-500) / <alpha-value>)",
+          600: "rgb(var(--tw-color-secondary-600) / <alpha-value>)",
+          700: "rgb(var(--tw-color-secondary-700) / <alpha-value>)",
+          800: "rgb(var(--tw-color-secondary-800) / <alpha-value>)",
+          900: "rgb(var(--tw-color-secondary-900) / <alpha-value>)",
+        },
+        dark: "#222222",
+      },
+      keyframes: {
+        flicker: {
+          "0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%": {
+            opacity: "0.99",
+            filter:
+              "drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))",
+          },
+          "20%, 21.999%, 63%, 63.999%, 65%, 69.999%": {
+            opacity: "0.4",
+            filter: "none",
+          },
+        },
+        shimmer: {
+          "0%": {
+            backgroundPosition: "-700px 0",
+          },
+          "100%": {
+            backgroundPosition: "700px 0",
+          },
+        },
+      },
+      animation: {
+        flicker: "flicker 3s linear infinite",
+        shimmer: "shimmer 1.3s linear infinite",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+} satisfies Config;
